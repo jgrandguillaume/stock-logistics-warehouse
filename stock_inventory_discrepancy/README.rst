5ae1f06bdfbf447f08639718d4e7525454436d3b
lreficent <lois.rilo@eficent.com>
2017-02-13 17:25:37 +0100

    [ADD] Latest changes in inventory_discrepancy.

--- stock_inventory_discrepancy/README.rst
+++ stock_inventory_discrepancy/README.rst
@@ -8,25 +8,21 @@ Stock Inventory Discrepancy
 
 Adds the capability to show the discrepancy of every line in an inventory and
 to block the inventory validation (setting it as 'Pending to Approve') when the
-discrepancy is over a user defined threshold. Only new group "Inventory /
-Control Manager" will be able to force the validation of those blocked
-inventories.
+discrepancy is greater than a user defined threshold.
 
+Only new group "Validate All inventory Adjustments" will be able to force the
+validation of those blocked inventories. By default Stock manager will belong
+to this group. In addition, Stock Users can validate inventories under the
+threshold now.
 
-Installation
-============
-
-To install this module, you need to:
-
-* Download this module to your addons path.
-* Install the module in your database.
 
 Configuration
 =============
 
-You can configure the rules to compute the cycle count, acting as follow:
+You can configure the threshold as described below:
 
-#. Go to "Inventory > Warehouse Management" > Warehouses (or Locations)".
+#. Go to "Inventory > Warehouse Management" > Warehouses" or to "Inventory >
+   Warehouse Management" > Locations".
 #. Modify the "Maximum Discrepancy Rate Threshold" either in a Warehouse or
    in a location. If set to 0.0 the threshold is disabled.
 
@@ -36,29 +32,26 @@ Usage
 If you configured a "Maximum Discrepancy Rate Threshold".
 
 * When validating an Inventory Adjustment if some line exceed the Discrepancy
-  Threshold the system will raise an user error and set the inventory's state
-  to 'Pending to Approve'.
+  Threshold the system will set the inventory's state to 'Pending to Approve'
+  and show the quantity of lines that exceed the threshold.
 * If both WH and location thresholds are configured, the location one has
   preference.
-* The warehouse control manager can force the validation of an inventory
-  pending to approve.
+* The user with "Validate All inventory Adjustments" rights can force the
+  validation of an inventory pending to approve.
 
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
    :target: https://runbot.odoo-community.org/runbot/153/9.0
 
-.. repo_id is available in https://github.com/OCA/stock-logistics-warehouse
-.. branch is "9.0" for example
-
 
 Bug Tracker
 ===========
 
 Bugs are tracked on `GitHub Issues
-<https://github.com/OCA/{project_repo}/issues>`_. In case of trouble, please
-check there if your issue has already been reported. If you spotted it first,
-help us smash it by providing detailed and welcomed feedback.
+<https://github.com/OCA/stock-logistics-warehouse/issues>`_. In case of
+trouble, please check there if your issue has already been reported. If you
+spotted it first, help us smash it by providing detailed and welcomed feedback.
 
 
 Images
--- stock_inventory_discrepancy/tests/test_inventory_discrepancy.py
+++ stock_inventory_discrepancy/tests/test_inventory_discrepancy.py
@@ -13,6 +13,7 @@ class TestInventoryDiscrepancy(TransactionCase):
         self.obj_location = self.env['stock.location']
         self.obj_inventory = self.env['stock.inventory']
         self.obj_product = self.env['product.product']
+        self.obj_warehouse = self.env['stock.warehouse']
 
         self.product1 = self.obj_product.create({
             'name': 'Test Product 1',
@@ -26,7 +27,26 @@ class TestInventoryDiscrepancy(TransactionCase):
         })
         self.test_loc = self.obj_location.create({
             'name': 'Test Location',
-            'usage': 'internal'
+            'usage': 'internal',
+            'discrepancy_threshold': 0.1
+        })
+        self.test_wh = self.obj_warehouse.create({
+            'name': 'Test WH',
+            'code': 'T',
+            'discrepancy_threshold': 0.2
+        })
+        self.obj_location._parent_store_compute()
+
+        # Create Stock manager able to force validation on inventories.
+        group_stock_man = self.env.ref('stock.group_stock_manager')
+        group_inventory_all = self.env.ref(
+            'stock_inventory_discrepancy.'
+            'group_stock_inventory_validation_always')
+        self.user = self.env['res.users'].create({
+            'name': 'Test User',
+            'login': 'user',
+            'email': 'test.user@example.com',
+            'groups_id': [(6, 0, [group_stock_man.id, group_inventory_all.id])]
         })
 
         starting_inv = self.obj_inventory.create({
@@ -79,3 +99,53 @@ class TestInventoryDiscrepancy(TransactionCase):
                          'Wrong Discrepancy qty computation.')
         self.assertEqual(inventory.line_ids[1].discrepancy_qty, - 1.0,
                          'Wrong Discrepancy qty computation.')
+
+    def test_discrepancy_validation(self):
+        """Tests the new workflow"""
+        inventory = self.obj_inventory.create({
+            'name': 'Test Forcing Validation Method',
+            'location_id': self.test_loc.id,
+            'filter': 'none',
+            'line_ids': [
+                (0, 0, {
+                    'product_id': self.product1.id,
+                    'product_uom_id': self.env.ref(
+                        "product.product_uom_unit").id,
+                    'product_qty': 3.0,
+                    'location_id': self.test_loc.id,
+                }),
+            ],
+        })
+        self.assertEqual(inventory.state, 'draft',
+                         'Testing Inventory wrongly configurated')
+        self.assertEqual(inventory.line_ids.discrepancy_threshold, 0.1,
+                         'Threshold wrongly computed in Inventory Line.')
+        inventory.action_done()
+        self.assertEqual(inventory.over_discrepancy_line_count, 1,
+                         'Computation of over-discrepancies failed.')
+        self.assertEqual(inventory.state, 'pending',
+                         'Inventory Adjustment not changing to Pending to '
+                         'Approve.')
+        inventory.sudo(self.user).action_force_done()
+        self.assertEqual(inventory.state, 'done',
+                         'Forcing the validation of the inventory adjustment '
+                         'not working properly.')
+
+    def test_warehouse_threshold(self):
+        """Tests the behaviour if the threshold is set on the WH."""
+        inventory = self.obj_inventory.create({
+            'name': 'Test Threshold Defined in WH',
+            'location_id': self.test_wh.view_location_id.id,
+            'filter': 'none',
+            'line_ids': [
+                (0, 0, {
+                    'product_id': self.product1.id,
+                    'product_uom_id': self.env.ref(
+                        "product.product_uom_unit").id,
+                    'product_qty': 3.0,
+                    'location_id': self.test_wh.lot_stock_id.id,
+                }),
+            ],
+        })
+        self.assertEqual(inventory.line_ids.discrepancy_threshold, 0.2,
+                         'Threshold wrongly computed in Inventory Line.')
