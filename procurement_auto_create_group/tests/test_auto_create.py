66675eeb5671ecb4b63cf23209a261490b2be751
lreficent <lois.rilo@eficent.com>
2018-01-11 12:27:15 +0100

    [10.0][MIG] procurement_auto_create_group

--- procurement_auto_create_group/tests/test_auto_create.py
+++ procurement_auto_create_group/tests/test_auto_create.py
@@ -2,7 +2,7 @@
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestProcurementAutoCreateGroup(TransactionCase):
